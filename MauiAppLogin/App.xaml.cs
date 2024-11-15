1bc9102c7356200bb28722308a2402ea62abc580
GuiiiSantoos <guilhermesantos131123@gmail.com>
2024-11-15 18:36:01 -0300

    add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -10,6 +10,12 @@
         }
         protected override Window CreateWindow(IActivationState activationState)
         {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 700;
+
+            return window;
 
         }
 
