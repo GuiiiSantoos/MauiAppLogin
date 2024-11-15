a36a10fd14825b6cd4e584fe15b35e7a8e43a5a0
GuiiiSantoos <guilhermesantos131123@gmail.com>
2024-11-15 18:15:03 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppLogin/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiAppLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+
+        }
+
+
+    }//fecha classe
+}//fecha namespace
